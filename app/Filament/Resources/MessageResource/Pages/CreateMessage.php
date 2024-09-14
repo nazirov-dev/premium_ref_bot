06c599bbac28af08bfa937682f18f47f2c8a1c22
nazirov-dev <nazirovdev@gmail.com>
2024-09-14 16:29:16 +0500

    other 5% processing

--- app/Filament/Resources/MessageResource/Pages/CreateMessage.php
+++ app/Filament/Resources/MessageResource/Pages/CreateMessage.php
@@ -6,12 +6,14 @@ use App\Filament\Resources\MessageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use App\Http\Controllers\TextController;
+use Illuminate\Support\Facades\Log;
 
 class CreateMessage extends CreateRecord
 {
     protected static string $resource = MessageResource::class;
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        Log::info('CreateMessage::mutateFormDataBeforeCreate: ', $data);
         $cleaned = TextController::sanitizeHtmlForTelegram($data['text']);
         $data['text'] = $cleaned;
         return $data;
