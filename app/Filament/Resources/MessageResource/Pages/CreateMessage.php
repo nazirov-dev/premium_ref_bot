86b1cdb5697f1efcd2243ceb5514606672f5b06d
nazirov-dev <nazirovdev@gmail.com>
2024-09-14 16:18:43 +0500

    other 5% processing

--- app/Filament/Resources/MessageResource/Pages/CreateMessage.php
+++ app/Filament/Resources/MessageResource/Pages/CreateMessage.php
@@ -5,8 +5,15 @@ namespace App\Filament\Resources\MessageResource\Pages;
 use App\Filament\Resources\MessageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use App\Http\Controllers\TextController;
 
 class CreateMessage extends CreateRecord
 {
     protected static string $resource = MessageResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $cleaned = TextController::sanitizeHtmlForTelegram($data['text']);
+        $data['text'] = $cleaned;
+        return $data;
+    }
 }
--- app/Filament/Resources/MessageResource/Pages/EditMessage.php
+++ app/Filament/Resources/MessageResource/Pages/EditMessage.php
@@ -5,11 +5,19 @@ namespace App\Filament\Resources\MessageResource\Pages;
 use App\Filament\Resources\MessageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use App\Http\Controllers\TextController;
 
 class EditMessage extends EditRecord
 {
     protected static string $resource = MessageResource::class;
 
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $cleaned = TextController::sanitizeHtmlForTelegram($data['text']);
+        $data['text'] = $cleaned;
+
+        return $data;
+    }
     protected function getHeaderActions(): array
     {
         return [
