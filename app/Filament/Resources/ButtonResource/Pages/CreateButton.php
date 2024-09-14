35ebfbb3782fcc42a80dfd6a6cabfed3b537d7ab
nazirov-dev <nazirovdev@gmail.com>
2024-09-14 20:57:41 +0500

    other 5% processing

--- app/Filament/Resources/ButtonResource/Pages/CreateButton.php
+++ app/Filament/Resources/ButtonResource/Pages/CreateButton.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\ButtonResource\Pages;
 use App\Filament\Resources\ButtonResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Log;
 
 class CreateButton extends CreateRecord
 {
     protected static string $resource = ButtonResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        Log::info('Info: ', $data);
+        return $data;
+    }
 }
