4e15b3babafda5f58f9904ba203fd6db44ed8df8
nazirov-dev <nazirovdev@gmail.com>
2024-09-14 20:53:21 +0500

    other 5% processing

--- app/Filament/Resources/MessageResource/Pages/CreateMessage.php
+++ app/Filament/Resources/MessageResource/Pages/CreateMessage.php
@@ -6,15 +6,49 @@ use App\Filament\Resources\MessageResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use App\Http\Controllers\TextController;
-use Illuminate\Support\Facades\Log;
 
 class CreateMessage extends CreateRecord
 {
     protected static string $resource = MessageResource::class;
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        Log::info('CreateMessage::mutateFormDataBeforeCreate: ', $data);
+        function convertToTelegramInlineKeyboard($text, $limitPerRow = 5)
+        {
+            $keyboard = [];
+            $key = [];
+
+            // Split the input into rows based on newlines
+            $rows = explode("\n", $text);
+
+            foreach ($rows as $row) {
+                // Match [text-url] pattern
+                preg_match_all('/\[(.*?)\-(.*?)\]/', $row, $matches, PREG_SET_ORDER);
+
+                foreach ($matches as $match) {
+                    $text = $match[1];
+                    $url = $match[2];
+
+                    // Add button to current row
+                    $key[] = ["text" => $text, "url" => $url];
+
+                    // If row reaches limit, add to keyboard and start a new row
+                    if (count($key) >= $limitPerRow) {
+                        $keyboard[] = $key;
+                        $key = [];
+                    }
+                }
+
+                // Add remaining buttons in the row
+                if (!empty($key)) {
+                    $keyboard[] = $key;
+                    $key = [];
+                }
+            }
+
+            return json_encode($keyboard);
+        }
         $cleaned = TextController::sanitizeHtmlForTelegram($data['text']);
+        $data['reply_markup'] = convertToTelegramInlineKeyboard($data['buttons']);
         $data['text'] = $cleaned;
         return $data;
     }
--- app/Filament/Resources/MessageResource/Pages/EditMessage.php
+++ app/Filament/Resources/MessageResource/Pages/EditMessage.php
@@ -14,7 +14,42 @@ class EditMessage extends EditRecord
 
     protected function mutateFormDataBeforeSave(array $data): array
     {
-        Log::info('EditMessage::mutateFormDataBeforeSave: ', $data);
+        function convertToTelegramInlineKeyboard($text, $limitPerRow = 5)
+        {
+            $keyboard = [];
+            $key = [];
+
+            // Split the input into rows based on newlines
+            $rows = explode("\n", $text);
+
+            foreach ($rows as $row) {
+                // Match [text-url] pattern
+                preg_match_all('/\[(.*?)\-(.*?)\]/', $row, $matches, PREG_SET_ORDER);
+
+                foreach ($matches as $match) {
+                    $text = $match[1];
+                    $url = $match[2];
+
+                    // Add button to current row
+                    $key[] = ["text" => $text, "url" => $url];
+
+                    // If row reaches limit, add to keyboard and start a new row
+                    if (count($key) >= $limitPerRow) {
+                        $keyboard[] = $key;
+                        $key = [];
+                    }
+                }
+
+                // Add remaining buttons in the row
+                if (!empty($key)) {
+                    $keyboard[] = $key;
+                    $key = [];
+                }
+            }
+
+            return json_encode($keyboard);
+        }
+        $data['reply_markup'] = convertToTelegramInlineKeyboard($data['buttons']);
         $cleaned = TextController::sanitizeHtmlForTelegram($data['text']);
         $data['text'] = $cleaned;
 
