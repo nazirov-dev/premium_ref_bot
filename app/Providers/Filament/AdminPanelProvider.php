3d24296ea6907d585e7712721932be73dd99aa53
nazirov-dev <nazirovdev@gmail.com>
2024-09-15 23:45:37 +0500

    adding daily bonust status resetter

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,6 +33,8 @@ class AdminPanelProvider extends PanelProvider
             ->globalSearch()
             ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
             ->emailVerification(isRequired:false)
+            ->sidebarCollapsibleOnDesktop()
+            ->topNavigation()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
